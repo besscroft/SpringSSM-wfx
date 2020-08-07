d0fc7f2f3a74bfc901be6e4716aaad682a5827df
besscroft <631908942@qq.com>
2020-08-07 19:35:32 +0800

    update

---
+++ src/main/java/com/bess/dao/UserDAO.java
@@ -0,0 +1,8 @@
+package com.bess.dao;
+
+/**
+ * @Author Bess Croft
+ * @DateTime 2020/8/7 18:05
+ */
+public interface UserDAO {
+}
---
+++ src/main/java/com/bess/pojo/User.java
@@ -0,0 +1,8 @@
+package com.bess.pojo;
+
+/**
+ * @Author Bess Croft
+ * @DateTime 2020/8/7 18:04
+ */
+public class User {
+}
